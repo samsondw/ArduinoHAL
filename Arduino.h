f471fa04f6f5fabb813d9f1767da298ad31977b1
rbohne <rbohne@localhost>
2011-06-04 20:01:33 +0000

    added some basic wiring functions

--- Arduino.h
+++ Arduino.h
@@ -3,19 +3,7 @@
 
 #include "mbed.h"
 
-#define HIGH 0x1
-#define LOW  0x0
-
-#define INPUT 0x0
-#define OUTPUT 0x1
-
-//#define true 0x1
-//#define false 0x0
-
-
-void pinMode(int, int);
-void digitalWrite(int, int);
-int digitalRead(int);
+#include "wiring.h"
 
 #endif
         

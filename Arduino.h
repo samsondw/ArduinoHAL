e682681604532c8cf993d000942a65679a72d882
Sam Weaver <samson_dw@hotmail.com>
2019-04-12 15:16:09 -0400

    Update Arduino.h

--- Arduino.h
+++ Arduino.h
@@ -7,12 +7,13 @@
 #define PROGMEM
 #define PSTR(STR) STR
 #endif
-
+/*
 #ifdef __AVR__
   #include <avr/pgmspace.h>
 #elif defined(ESP8266) || defined(ESP32)
   #include <pgmspace.h>
 #endif
+*/
 
 #define ARDUINO 101
 

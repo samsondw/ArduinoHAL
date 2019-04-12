81c695cab16e4a19a6c91151efac1981ff21a93b
Sam Weaver <samson_dw@hotmail.com>
2019-04-12 11:24:20 -0400

    Update Arduino.h

--- Arduino.h
+++ Arduino.h
@@ -3,6 +3,17 @@
 
 #include "mbed.h"
 
+#if defined(__arm__) && !defined(PROGMEM)
+#define PROGMEM
+#define PSTR(STR) STR
+#endif
+
+#ifdef __AVR__
+  #include <avr/pgmspace.h>
+#elif defined(ESP8266) || defined(ESP32)
+  #include <pgmspace.h>
+#endif
+
 #include "wiring.h"
 
 #endif

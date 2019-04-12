c983b1bdbef175aeb9702dc35ee264ed3ca81e1c
Sam Weaver <samson_dw@hotmail.com>
2019-04-12 13:46:16 -0400

    Update Print.h

--- Print.h
+++ Print.h
@@ -2,10 +2,21 @@
 #define Print_h
 
 
-#define Print Stream
-
 #include <string>
 #define String std::string
+
+class Print : public Stream
+{
+public:
+
+int _putc(int value)
+{
+    write((uint8_t)value);
+    return value;
+}
+int _getc() { return -1; }
+
+}
 
 
 #endif

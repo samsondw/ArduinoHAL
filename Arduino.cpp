cba89a2a6a0721eb9cba4ae3a530d0e7d62f9114
Sam Weaver <samson_dw@hotmail.com>
2019-04-12 13:51:35 -0400

    Update Arduino.cpp

--- Arduino.cpp
+++ Arduino.cpp
@@ -3,7 +3,9 @@
 
 
 
-static DigitalInOut allpins[] = {LED1, LED2, LED3, LED4, NC, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30};
+//static DigitalInOut allpins[] = {LED1, LED2, LED3, LED4, NC, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30};
+static DigitalInOut allpins[] = {LED1, LED2, LED3, LED4, NC, NC, NC, NC, NC, NC, NC, NC, NC, NC, NC, NC, NC, NC, NC, NC, NC, NC, NC, NC, NC, NC, NC, NC, NC, NC, NC};
+
 
 void digitalWrite(int pin, int value) 
 {
